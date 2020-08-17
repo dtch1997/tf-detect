ee0121747a19e64dff26e7fd957a2ee7b4a4de86
Daniel Tan <dtch009@gmail.com>
2020-08-17 07:26:31 +0000

    Modify conversion script to use fake dataset

--- src/convert_tf_model_to_tf_lite.py
+++ src/convert_tf_model_to_tf_lite.py
@@ -1,37 +1,58 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import argparse
+import numpy as np
 import pathlib
+
+from PIL import Image
 
 parser = argparse.ArgumentParser(description="Convert a TF SavedModel to a TFLite model")
 parser.add_argument("--model-name", help="Name of the model. See tools/train_model.sh for semantics of model name")
-parser.add_argument("--dataset", help="Name of the TFRecord dataset that should be used for quantization") 
-parser.add_argument("--num-samples", help="Number of samples to calibrate on")
+parser.add_argument("--dataset", help="Name of the TFRecord dataset that should be used for quantization", default="crowdhuman_debug") 
+parser.add_argument("--num-samples", help="Number of samples to calibrate on", default=100)
 
-args = parser.parse_args()
-model_savedir = f'exported_models/{args.model_name}/saved_model'
-data_dir = f'data/tfrecord/{args.dataset}'
+def fake_data_gen(num_samples):
+    def representative_dataset_gen():
+        for i in range(num_samples):
+            yield [np.ones(1, 96, 96, 3).astype(np.uint8)]
+    return representative_dataset_gen
 
-def get_filenames(datadir: str):
-    datadirpath = pathlib.Path(datadir)
-    filenames = [p for p in datadirpath.iterdir() if
-            (not p.is_dir() and \
-             p.suffix == ".record")]
-    return filenames
+def make_data_gen(dataset_name, num_samples):
+    """
+    Uses the images from datadir/Images to quantize the model. 
+    """
+    if dataset_name == "fake":
+        return fake_data_gen(num_samples)
+    
+    datadir = f'data/raw/{dataset_name}'
+    imgdir = datadir / 'Images'
+    
+    def representative_dataset_gen():
+        for i, filename in enumerate(imgdir.iterdir()):
+            if filename.suffix not in ['.jpeg', '.jpg', '.png']: 
+                continue
+            image = Image.open(str(filename.resolve()))
+            image = image.resize((96, 96))
+            yield [np.array(image).reshape(-1, 96, 96, 3)]
+            if i >= num_samples: break
+    return representative_dataset_gen
 
-def representative_dataset_gen():
-    dataset = tf.data.TFRecordDataset(get_filenames(data_dir))
-    for raw_record in dataset.take(args.num_samples):
-        example = tf.train.Example()
-        example.ParseFromString(raw_record.numpy())
+def main():
+    args = parser.parse_args()
+    model_savedir = f'exported_models/{args.model_name}/saved_model'
 
+    converter = tf.lite.TFLiteConverter.from_saved_model(model_savedir, signature_keys=['serving_default'])
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.experimental_new_converter = True
+    converter.representative_dataset = make_data_gen(args.dataset, args.num_samples)
+    # Ensure that if any ops can't be quantized, the converter throws an error
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    # Set the input and output tensors to uint8 (APIs added in r2.3)
+    converter.inference_input_type = tf.uint8
+    converter.inference_output_type = tf.uint8
+    quantized_model = converter.convert()
+    bytes = open(f'exported_models/{args.model_name}/model.tflite', "wb").write(quantized_model)
 
-representative_dataset_gen()
-
-converter = tf.lite.TFLiteConverter.from_saved_model(model_savedir, signature_keys=['serving_default'])
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.experimental_new_converter = True
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
-quantized_model = converter.convert()
-bytes = open(f'exported_models/{args.model_name}/model.tflite', "wb").write(quantized_model)
-
-print(f"Model of {bytes} bytes was written")
+    
+if __name__ == "__main__":
+    main()
